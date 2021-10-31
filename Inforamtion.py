d863406021888ad1a94f431c808a474d7dc1cca1
Slane123 <92850090+Slane123@users.noreply.github.com>
2021-10-31 22:55:05 +0000

    Create Inforamtion.py

---
+++ Inforamtion.py
@@ -0,0 +1,6 @@
+class user_information:
+    def __init__(self, name, sign, difficulty, gamemode):
+        self.name = name
+        self.sign = sign
+        self.difficulty = difficulty
+        self.gamemode = gamemode
