fa06429e38e687a96846e309cb5eb413c69d35c1
Slane123 <92850090+Slane123@users.noreply.github.com>
2021-10-31 22:54:02 +0000

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,8 @@
+class game_variables:
+    def __init__(self, answer, response, equation, score, wrong, time_taken):
+        self.answer = answer
+        self.response = response
+        self.equation = equation
+        self.score = score
+        self.wrong = wrong
+        self.time_taken = time_taken
